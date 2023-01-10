4ea48a0443854247630e5457aa0df03894dbc099
Michael Okoro <michaelokoro1@gmail.com>
2023-01-10 14:08:11 +0100

    Update HBP.py

--- HBP.py
+++ HBP.py
@@ -2,7 +2,6 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 
